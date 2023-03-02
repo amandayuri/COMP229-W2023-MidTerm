8b9e50b8bd05048d88c4a2ba6141199338f96ca6
Amanda Ike <amandayurike@gmail.com>
2023-03-01 21:58:19 -0500

    applied correction

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+// ------- HEADER ------------
+// Filename: db.js
+// Student name: Amanda Yuri Monteiro Ike
+// Student ID: 301257019
+// Web App name: COMP229-W2023-MidTerm-301257019
+
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://comp229-lab:xpftrQAJi3SCZNrA@mongodbserver.ghhuqpr.mongodb.net/bookstore?retryWrites=true&w=majority"
+
+};
